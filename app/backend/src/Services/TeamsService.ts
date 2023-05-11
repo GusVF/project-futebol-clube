76887ce3a19224a8ddc5ecd42ed086dedcc6f3e7
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-05-11 15:34:59 -0300

    fixing service and controller

--- app/backend/src/Services/TeamsService.ts
+++ app/backend/src/Services/TeamsService.ts
@@ -3,9 +3,6 @@ import TeamsModel, { teamsAttributes } from '../database/models/TeamsModel';
 export default class TeamsService {
   public static async findAll(): Promise<teamsAttributes[]> {
     const allTeams = await TeamsModel.findAll();
-    if (!allTeams) {
-      throw new Error('Teams not found');
-    }
     return allTeams;
   }
 }
