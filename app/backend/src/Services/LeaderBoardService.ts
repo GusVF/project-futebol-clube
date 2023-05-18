c9b971e7f73a90ba0ea230a9d90ce96b31e5978d
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-05-18 13:56:10 -0300

    requisito-24-25

--- app/backend/src/Services/LeaderBoardService.ts
+++ app/backend/src/Services/LeaderBoardService.ts
@@ -17,18 +17,29 @@ export default class LeaderBoardService {
         totalLosses: this.getTotalLosses(matchStats),
         goalsFavor: this.getGoalsFavor(matchStats),
         goalsOwn: this.getGoalsOwn(matchStats),
+        goalsBalance: this.getGoalsBalance(matchStats),
+        efficiency: this.getEficiency(matchStats),
       };
     }));
   }
 
   public static getTotalPoints(matchStats: matchesAttributes[]) {
-    return matchStats.reduce((acc, curr) =>
-      (curr.homeTeamGoals < curr.awayTeamGoals ? acc + 0 : acc + curr.homeTeamGoals), 0);
+    const result = matchStats.reduce((acc, curr) => {
+      if (curr.homeTeamGoals < curr.awayTeamGoals) {
+        return acc;
+      }
+      if (curr.homeTeamGoals > curr.awayTeamGoals) {
+        const points = acc + 3;
+        return points;
+      }
+      return acc + 1;
+    }, 0);
+    return result;
   }
 
   public static getTotalVictories(matchStats: matchesAttributes[]) {
     return matchStats.reduce((acc, curr) =>
-      (curr.homeTeamGoals < curr.awayTeamGoals ? acc + 0 : acc + 1), 0);
+      (curr.homeTeamGoals > curr.awayTeamGoals ? acc + 1 : acc), 0);
   }
 
   public static getTotalDraws(matchStats: matchesAttributes[]) {
@@ -49,5 +60,34 @@ export default class LeaderBoardService {
   public static getGoalsOwn(matchStats: matchesAttributes[]) {
     return matchStats.reduce((acc, curr) =>
       (acc + curr.awayTeamGoals), 0);
+  }
+
+  public static getGoalsBalance(matchStats: matchesAttributes[]) {
+    const goalsBalance = matchStats.reduce((acc, curr) =>
+      acc + (curr.homeTeamGoals - curr.awayTeamGoals), 0);
+    return goalsBalance;
+  }
+
+  public static getEficiency(matchStats: matchesAttributes[]) {
+    const totalPoints = this.getTotalPoints(matchStats);
+    const totalGames = matchStats.length;
+    const result = ((totalPoints / (totalGames * 3)) * 100).toFixed(2);
+    return result;
+  }
+
+  public static async sortAllStats() {
+    const sortAllStats = await this.getAllStats();
+    sortAllStats.sort((b, a) => {
+      if (a.totalPoints === b.totalPoints) {
+        if (a.totalVictories === b.totalVictories) {
+          if (a.goalsBalance === b.goalsBalance) {
+            return a.goalsFavor - b.goalsFavor;
+          }
+          return a.goalsBalance - b.goalsBalance;
+        }
+        return a.totalVictories - b.totalVictories;
+      }
+      return a.totalPoints - b.totalPoints;
+    }); return sortAllStats;
   }
 }
--- app/backend/src/controllers/LearderBoardController.ts
+++ app/backend/src/controllers/LearderBoardController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import LeaderBoardService from '../Services/LeaderBoardService';
 
 async function getAllStats(_req: Request, res: Response) {
-  const matchStats = await LeaderBoardService.getAllStats();
+  const matchStats = await LeaderBoardService.sortAllStats();
   return res.status(200).json(matchStats);
 }
 
